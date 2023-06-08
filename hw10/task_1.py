67d5636059b5852d577338cf7900bd5afc1d0c3a
AnnaBlem <annblem@gmail.com>
2023-06-08 09:51:30 +0300

    решено задание 1

---
+++ hw10/task_1.py
@@ -0,0 +1,28 @@
+# Напишите генератор generate_random_name(), используя модуль random,
+# который генерирует два слова из латинских букв от 1 до 15 символов, разделенных пробелами
+# Например при исполнении следующего кода:
+# gen = generate_random_name()
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+#
+# Выводится:
+# tahxmckzexgdyt ocapwy
+# dxqebbukr jg
+# aym jpvezfqexlv
+# iuy qnikkgxvxfxtxv
+
+import random
+
+
+# Здесь пишем код
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z']
+
+
+def generate_random_name():
+    while True:
+        first_word = ''.join(random.sample(letters, random.randrange(1, 15)))
+        second_word = ''.join(random.sample(letters, random.randrange(1, 15)))
+        yield first_word + ' ' + second_word
