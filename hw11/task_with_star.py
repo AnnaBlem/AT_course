ea0eb3e21c5d3df0f4388be25204fc26282c12e7
AnnaBlem <annblem@gmail.com>
2023-06-13 23:11:37 +0300

    решено задание 3

---
+++ hw11/task_with_star.py
@@ -0,0 +1,46 @@
+# Перейти на  https://sbis.ru/
+# В Footer'e найти "Скачать СБИС"
+# Перейти по ней
+# Скачать СБИС Плагин для вашей ОС в папку с данным тестом
+# Убедиться, что плагин скачался
+# Вывести на печать размер скачанного файла в мегабайтах
+# Для сдачи задания пришлите код и запись с экрана прохождения теста
+import os.path
+
+import urllib.request
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+driver = webdriver.Chrome()
+site = 'https://sbis.ru/'
+
+try:
+    print('Перейти на  https://sbis.ru/')
+    driver.get(site)
+    sleep(5)
+
+    print('В Footer найти "Скачать СБИС"')
+    download_button = driver.find_element(By.LINK_TEXT, 'Скачать СБИС')
+
+    print('Перейти по "Скачать СБИС"')
+    driver.execute_script("arguments[0].click();", download_button)
+    sleep(5)
+
+    print('Скачать СБИС Плагин для вашей ОС в папку с данным тестом')
+    plugin_button = driver.find_element(By.CSS_SELECTOR, '[data-id="plugin"]')
+    plugin_button.click()
+    sleep(5)
+    web_setup_url = [a for a in driver.find_elements(By.CSS_SELECTOR, "a.sbis_ru-DownloadNew-loadLink__link")
+                     if a.is_displayed()][0].get_attribute("href")
+    download_path = os.path.join(os.path.dirname(__file__), "sbisplugin-setup-web.exe")
+    urllib.request.urlretrieve(web_setup_url, download_path)
+
+    print('Убедиться, что плагин скачался')
+    assert os.path.exists(download_path), "Файл не скачался"
+
+    print("Размер плагина:", os.path.getsize(download_path) // (1024 * 1024), "Мб")
+
+finally:
+    driver.quit()
