6e1b4171c4da0c12eba2d91736c3808f76c24dd3
Anna Blem <annblem@gmail.com>
2023-06-17 15:29:56 +0300

    исправлено задание 1

--- hw11/task_1.py
+++ hw11/task_1.py
@@ -11,35 +11,36 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-sleep(3)
 try:
     print('Перейти на https://sbis.ru/')
     driver.get("https://sbis.ru/")
     sleep(1)
 
     print('Перейти в раздел "Контакты"')
-    Contacts = driver.find_element(By.CSS_SELECTOR, '.sbisru-Header__menu-item-1')
-    Contacts.click()
+    contacts = driver.find_element(By.CSS_SELECTOR, '.sbisru-Header__menu-item-1')
+    assert contacts.text == 'Контакты'
+    contacts.click()
     sleep(1)
 
     print('Найти баннер Тензор, кликнуть по нему')
-    Banner = driver.find_element(By.CSS_SELECTOR, '[alt="Разработчик системы СБИС — компания «Тензор»"]')
-    Banner.click()
+    banner = driver.find_element(By.CSS_SELECTOR, '[alt="Разработчик системы СБИС — компания «Тензор»"]')
+    banner.click()
     sleep(1)
 
     print('Перейти на https://tensor.ru/')
     driver.switch_to.window(driver.window_handles[1])
+    assert driver.current_url == 'https://tensor.ru/', 'Страница https://tensor.ru/ не открылась'
 
     print('Проверить, что есть блок новости "Сила в людях"')
-    Strength_in_people = driver.find_elements(By.CSS_SELECTOR, '.tensor_ru-pb-16')[1]
-    assert Strength_in_people.is_displayed(), '"Сила в людях" не отображается'
+    strength_in_people = driver.find_element(By.XPATH, '//p[text()="Сила в людях"]')
+    assert strength_in_people.is_displayed(), '"Сила в людях" не отображается'
     sleep(3)
 
     print('Перейдите в этом блоке в "Подробнее" и убедитесь, что открывается https://tensor.ru/about')
-    About = driver.find_element(By.CSS_SELECTOR, '.tensor_ru-Index__card-text [href="/about"]')
-    driver.execute_script("return arguments[0].scrollIntoView(true);", About)
+    about = driver.find_element(By.XPATH, '//p[text()="Сила в людях"]/..//a[text()="Подробнее"]')
+    driver.execute_script("return arguments[0].scrollIntoView(true);", about)
     sleep(3)
-    About.click()
+    about.click()
     sleep(3)
     assert driver.current_url == 'https://tensor.ru/about', 'Подробнее не открылась'
     sleep(3)
